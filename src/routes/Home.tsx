8529943b0d540bace6e1cdbf3e53ad2e21ffe019
garahad <bfsudong@gmail.com>
2019-12-17 15:04:43 +0900

    Antd, emotion available
    made render page layout

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Row, Col, Card } from 'antd';
 
 function Home() {
-  return <div>헬친 렌더 페이지</div>;
+  return (
+    <div>
+      <Row gutter={24} type="flex" justify="space-between">
+        <Col xs={24} md={24}>
+          헬친 랜더 헬스 사진
+        </Col>
+
+        <Col xs={24} md={8}>
+          <Card title="Card title" bordered style={{ width: 300 }}>
+            <p>유저 1 카드</p>
+          </Card>
+        </Col>
+        <Col xs={24} md={8}>
+          <Card title="Card title" bordered style={{ width: 300 }}>
+            <p>유저 2 카드</p>
+          </Card>
+        </Col>
+        <Col xs={24} md={8}>
+          <Card title="Card title" bordered style={{ width: 300 }}>
+            <p>유저 3 카드</p>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default Home;
