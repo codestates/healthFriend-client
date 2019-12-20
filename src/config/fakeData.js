f97a35ddf802bf9c5500e099e93513a41309660f
garahad <bfsudong@gmail.com>
2019-12-20 21:22:29 +0900

    input callosel completed

---
+++ src/config/fakeData.js
@@ -0,0 +1,53 @@
+const fakeData = {
+  inputRegister: [
+    {
+      number: 1,
+      subject: 'day',
+      question: '운동이 가능한 요일은 언제인가요?',
+      answer: ['월', '화', '수', '목', '금', '토', '일'],
+    },
+    {
+      number: 2,
+      subject: 'place',
+      question: '운동 가능 지역은 어디신가요?',
+      answer: [],
+    },
+    {
+      number: 3,
+      subject: '3dae',
+      question: '3대 중량이 어떻게 되세요?',
+      answer: [
+        '3대가 뭔가요?(생초보)',
+        '100~200 (하수)',
+        '200~300 (중수)',
+        '300~400 (고수)',
+        '400이상 (괴물)',
+      ],
+    },
+    {
+      number: 4,
+      subject: 'reason',
+      question: '헬스 친구를 찾는 이유는?',
+      answer: [
+        '중량 강화',
+        '재밌게 운동하고 싶어서',
+        '친구 찾기',
+        '의지 부족을 이겨내고 싶어서',
+      ],
+    },
+    {
+      number: 5,
+      subject: 'gender',
+      question: '당신의 성별은?',
+      answer: ['남자', '여자'],
+    },
+    {
+      number: 6,
+      subject: 'introduce',
+      question: '마지막으로 헬친들에게 인사를 남겨주세요!!',
+      answer: [],
+    },
+  ],
+};
+
+export default fakeData;
