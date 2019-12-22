f817a75ebed85ac9e79cc6c110760c8d0f1f9c7a
양원석 <wonseokyang@cita.co.kr>
2019-12-22 14:16:55 +0900

    기능: 등록 페이지 select 기능 수정
    - 등록 페이지 select 기능 수정
    - 기타 스타일 작업

--- src/components/RegisterInput.tsx
+++ src/components/RegisterInput.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Checkbox, Input } from 'antd';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
@@ -8,35 +8,67 @@ import fakeData from '../config/fakeData';
 import PlaceSelect from './PlaceSelect'
 
 
+const wrapper = css`
+  margin-bottom: 20px;
+`;
+
+const checkboxDiv = css`
+  border: 1px solid #ededed;
+  border-radius: 5px;
+`;
+
 const checkboxDesign = css`
-  border: 0.5px solid gray;
+  border-bottom: 1px solid #ededed;
+  padding: 10px 20px;
   display: block;
-  height: 30px;
   line-height: 30px;
+  margin: 0 !important;
+
+  &:last-child {
+    border-bottom: 0;
+  }
 `;
 
 const { TextArea } = Input;
 
 type RegisterInputProps = {
   order: number;
+  checkArr: Array<boolean>;
+  setCheckArr: Function;
 };
 
-export default function RegisterInput({ order }: RegisterInputProps) {
+export default function RegisterInput({ order, checkArr, setCheckArr }: RegisterInputProps) {
   const currentInput = fakeData.inputRegister.filter(
     (elm) => elm.number === order,
   );
   const { question, answer, subject } = currentInput[0];
 
+  // 원석: question.slice(5)는 왜 있는건지 궁금합니다.
   let questionCheckboxes = answer.map((ele, idx) => (
-    <Checkbox name={`${question.slice(5)}${idx + 1}`} key={idx + 1} css={checkboxDesign} >
+    <Checkbox
+      name={`${question.slice(5)}${idx + 1}`}
+      key={idx + 1}
+      css={checkboxDesign}
+      checked={checkArr[idx]}
+      onChange={() => setCheckArr(answer.map((ele, i) => {
+        if (i === idx) {
+          return true;
+        }
+        return false;
+      }
+    ))}>
       {ele}
     </Checkbox>
   ));
 
   return (
-    <div>
+    <div css={wrapper}>
       <h2>{question}</h2>
-      {questionCheckboxes}
+      {(subject !== 'place' && subject !== 'introduce') && (
+        <div css={checkboxDiv}>
+          {questionCheckboxes}
+        </div>
+      )}
       {answer.length === 0 && subject === 'place' ? <PlaceSelect/> : null}
       {answer.length === 0 && subject === 'introduce' ? (
         <TextArea rows={4} />
