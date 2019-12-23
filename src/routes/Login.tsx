c1e648a42c85356593a24da6f307d73c083bbc03
garahad <bfsudong@gmail.com>
2019-12-24 08:39:29 +0900

    firebase congif delete

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,59 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import firebase from 'firebase';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import React /* { useState, useEffect } */ from 'react';
+// import firebase from 'firebase';
+// import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
-import config from '../config/firebaseConfig';
+// import config from '../config/firebaseConfig';
 
-firebase.initializeApp(config);
+// firebase.initializeApp(config);
 
 function Login() {
-  const [state, setState] = useState({
-    isSignedIn: false,
-  });
+  // const [state, setState] = useState({
+  //   isSignedIn: false,
+  // });
 
-  // console.log('config', config)
+  // const uiConfig = {
+  //   signInFlow: 'popup',
+  //   signInOptions: [
+  //     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  //     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+  //     firebase.auth.GithubAuthProvider.PROVIDER_ID,
+  //   ],
+  //   callbacks: {
+  //     signInSuccessWithAuthResult: () => false,
+  //   },
+  // };
 
-  const uiConfig = {
-    signInFlow: 'popup',
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    ],
-    callbacks: {
-      signInSuccessWithAuthResult: () => false,
-    },
-  };
+  // useEffect(() => {
+  //   const unregisterAuthObserver = firebase
+  //     .auth()
+  //     .onAuthStateChanged((user) => {
+  //       setState({ isSignedIn: !!user });
+  //     });
 
-  useEffect(() => {
-    const unregisterAuthObserver = firebase
-      .auth()
-      .onAuthStateChanged((user) => {
-        setState({ isSignedIn: !!user });
-      });
+  //   return () => {
+  //     unregisterAuthObserver();
+  //   };
+  // }, []);
 
-    return () => {
-      unregisterAuthObserver();
-    };
-  }, []);
-
-  if (state.isSignedIn) {
-    // console.log('firebase.auth().currentUser', firebase.auth().currentUser?.refreshToken);
-    firebase
-      .auth()
-      .currentUser!.getIdToken(/* forceRefresh */ true)
-      .then(function(idToken) {
-        // console.log('idToken', idToken);
-      })
-      .catch(function(error) {
-        // console.log(error);
-      });
-  }
+  // if (state.isSignedIn) {
+  //   // console.log('firebase.auth().currentUser', firebase.auth().currentUser?.refreshToken);
+  //   firebase
+  //     .auth()
+  //     .currentUser!.getIdToken(/* forceRefresh */ true)
+  //     .then(function(idToken) {
+  //       // console.log('idToken', idToken);
+  //     })
+  //     .catch(function(error) {
+  //       // console.log(error);
+  //     });
+  // }
   return (
     <div>
       서버 구글 로그인
       <a href="http://localhost:4000/auth/google">서버 구글 로그인</a>
-      {state.isSignedIn ? (
+      {/* {state.isSignedIn ? (
         <div>
           <div>
             {' '}
@@ -70,7 +68,7 @@ function Login() {
           uiConfig={uiConfig}
           firebaseAuth={firebase.auth()}
         />
-      )}
+      )} */}
     </div>
   );
 }
