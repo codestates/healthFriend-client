77d821d70e8e0230179335f6f97b221285d54926
garahad <bfsudong@gmail.com>
2020-01-01 21:39:20 +0900

    console 찍어서 다시 날려봄

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -35,7 +35,12 @@ const navLinkItem = css`
 // 로그인 과정에서 api call을 하면 cache에 이후 자동으로 저장되는건지도 확인
 
 export default function Header() {
-  const { data } = useQuery(GET_USERINFO);
+  const { data, error } = useQuery(GET_USERINFO);
+
+  console.log('data', data);
+  if (error) {
+    console.log('error', error.message);
+  }
 
   // const client = useApolloClient();
   // const todos = client.readQuery({
