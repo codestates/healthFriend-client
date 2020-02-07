e90c6d28ffae4fa36ae307016e3fb0e492d92359
sbinha0819 <sbinha123@gmail.com>
2020-02-07 12:18:29 +0900

    ignore streamConfig

--- src/config/streamConfig.tsx
+++
@@ -1,7 +0,0 @@
-export const APP_ID = 67459;
-export const API_KEY = '7gfp3v3jzxev';
-//4rcx837szctw
-//7gfp3v3jzxev
-export const SECRET =
-  '2r9f57cyapym62axq2uadzwbzehsqquxhrmngrhmjdqrzyajscz9enwe7dfdt7qv';
-export const PRODUCTION_APP = 'garahad';
